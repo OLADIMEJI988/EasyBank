ac397b1e21644777370d2cefdb3b990eb4cd0aae
OLADIMEJI988 <oladimejimax49@gmail.com>
2024-08-22 16:34:49 +0100

    added the js to the app file

--- dist/app.js
+++ dist/app.js
@@ -9,3 +9,10 @@
 //         navbar.classList.remove("active");
 //     }
 // }
+
+function Menu(e) {
+    let list = document.querySelector("ul");
+    e.name === "menu"
+      ? ((e.name = "close"), list.classList.add("opacity-100"))
+      : ((e.name = "menu"), list.classList.remove("opacity-100"));
+  }
