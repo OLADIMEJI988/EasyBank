8a2c25d9e8ba7cccb261e6275fba3c261f2a6cd4
OLADIMEJI988 <oladimejimax49@gmail.com>
2024-08-22 16:55:23 +0100

    removed a comment

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,3 @@
-// const hamburgerToggle = document.querySelector("#hamburger-active");
-// const navbar = document.querySelector("#navbar");
-// hamburgerToggle.addEventListener("click", hamburgerActive);
-
-// function hamburgerActive () {
-//     if(!navbar.classList.contains("active")){
-//         navbar.classList.add("active");
-//     } else {
-//         navbar.classList.remove("active");
-//     }
-// }
-
 function Menu(e) {
     let list = document.querySelector("ul");
     e.name === "menu"
